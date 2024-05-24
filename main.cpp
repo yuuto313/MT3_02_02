26d185142c6526e42dd78d33c23ce73b43c29099
ozawa yuuto <146906726+yuuto313@users.noreply.github.com>
2024-05-24 11:49:18 +0900

    三角形構造体追加

--- main.cpp
+++ main.cpp
@@ -42,6 +42,11 @@ struct  Segment
 	Vector3 diff;//終点への差分ベクトル
 };
 
+//三角形
+struct Triangle {
+	Vector3 vertices[3];//頂点
+};
+
 //クロス積（ベクトル積）
 Vector3 Cross(const Vector3& v1, const Vector3& v2) {
 	return Vector3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
